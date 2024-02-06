8dcda83f0ac861ee10201552f4765d6071e7bd3d
ritik <ritikbhatt020@gmail.com>
2024-02-07 04:13:44 +0530

    ✂️Made the api for Movies

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from tastypie.resources import ModelResource
+from movies.models import Movie
 
-# Create your models here.
+# Create your models here. In apis, we refer to the models as resources
+class MovieResource(ModelResource):  #it represents the concept of a Movie in a restful api
+    class Meta:  # tastypie looks for this inner class Meta
+        queryset = Movie.objects.all()
+        resource_name = 'movies'
+    
