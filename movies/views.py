dfc5da06c270a38d2a4cd331d59db9d094cd09fe
ritik <ritikbhatt020@gmail.com>
2024-02-07 02:47:27 +0530

    🔫Applied anchor tag to movie title

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 # Create your views here.
@@ -16,10 +16,12 @@ def index(request):
 
 
 def detail(request, movie_id):
-    try:
+    # try:
         # we can also use pk instead of id
-        movie = Movie.objects.get(id=movie_id)
+        # movie = Movie.objects.get(id=movie_id)
         # return HttpResponse(movie_id)
-        return render(request, 'movies/detail.html', {'movie': movie})
-    except Movie.DoesNotExist:
-        raise Http404
+        # return render(request, 'movies/detail.html', {'movie': movie})
+    # except Movie.DoesNotExist:
+        # raise Http404()
+    movie = get_object_or_404(Movie, id=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
