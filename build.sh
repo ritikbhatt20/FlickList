b8f68f2f3d5ffa21901aa267fac58dd26b557566
ritik <ritikbhatt020@gmail.com>
2024-02-09 01:21:57 +0530

    🧰changed files for build on vercel

--- build.sh
+++
@@ -1,6 +0,0 @@
-echo " CUSTOM BUILD START "
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-python3.9 manage.py migrate
-echo " CUSTOM BUILD END "
-
