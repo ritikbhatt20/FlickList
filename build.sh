686b1306b5a369cb752110e0f6ce326ddd502222
ritik <ritikbhatt020@gmail.com>
2024-02-09 00:36:49 +0530

    🔨BUILD

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 echo " CUSTOM BUILD START "
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
-python3 manage.py migrate
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py migrate
 echo " CUSTOM BUILD END "
 
