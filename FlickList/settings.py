e6464eb6b4125e59e02ab0f56b5232d4f0649c3c
ritik <ritikbhatt020@gmail.com>
2024-02-10 22:01:20 +0530

    ⚙️Updated settings for postgres

--- FlickList/settings.py
+++ FlickList/settings.py
@@ -82,8 +82,16 @@ WSGI_APPLICATION = 'FlickList.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # Database name
+        'USER': 'postgres',  # Database user
+        'PASSWORD': 'bhatt',      # Database password
+        'HOST': 'localhost', # Database host
+        'PORT': '5432',      # Database port
+        'OPTIONS': {
+            'connect_timeout': 10,
+            'sslmode': 'prefer',
+        }
     }
 }
 
