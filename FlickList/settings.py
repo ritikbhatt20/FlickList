3bce43b42f8ba0e3b4ee14d68d0909cd6595e1d4
ritik <ritikbhatt020@gmail.com>
2024-02-10 22:45:41 +0530

    🔨Updated settings.py for postgres

--- FlickList/settings.py
+++ FlickList/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',  # Database name
         'USER': 'postgres',  # Database user
-        'PASSWORD': '',      # Database password
+        'PASSWORD': 'bhatt',      # Database password
         'HOST': '127.0.0.1', # IPv4 loopback address
         'PORT': '5432',      # Database port
         'OPTIONS': {
